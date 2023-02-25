956a260941ba2ef948f2a3d2708b78bb4f99cfee
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-25 16:15:08 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO departments (department_name)
 VALUES ("Customer Service"),
        ("HR"),
        ("IT"),
-       ("Marketing");
+       ("Marketing"),
        ("Sales");
 
 INSERT INTO roles (title, salary, department_id)
@@ -11,11 +11,12 @@ VALUES ("Backend Web Developer", 90000, 3),
        ("Frontend Web Developer", 90000, 3),
        ("Full Stack Web Developer", 120000, 3),
        ("Human Resources Director", 70000, 2),
+       ("IT Director", 150000, 3),
        ("Marketing Manager", 70000, 4),
        ("Sales Associate", 70000, 5);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Jane", "Doe", 1, NULL),
-       ("Jack", "Doe", 3, NULL),
-       ("Josh", "Doe", 7, NULL),
-       ("John", "Doe", 7, 4);
+VALUES ("Jane", "Doe", 6, NULL),
+       ("Jack", "Smith", 3, 1),
+       ("Josh", "Perry", 1, 1),
+       ("John", "Doe", 5, NULL);
